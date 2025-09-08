2e4548844f43bf19bee8000c7f8fcc02fa68cd40
Md. Johirul islam <88481228+Rasel-pro@users.noreply.github.com>
2025-09-08 22:56:14 +0600

    shohel text add

--- lib/HomeScreen/home_screen.dart
+++ lib/HomeScreen/home_screen.dart
@@ -64,7 +64,7 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox( height: 22,),
-            Text('data'),
+            Text('shohel'),
           ],
         ),
       ),
