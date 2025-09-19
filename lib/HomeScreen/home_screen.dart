5e856f0ce020712bc6a3193a342f6419b5cb47f5
Adnan Nahid <133763369+nahidhasanniloy@users.noreply.github.com>
2025-09-19 15:55:16 +0600

    add production screen

--- lib/HomeScreen/home_screen.dart
+++ lib/HomeScreen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:startech/HomeScreen/login_screen.dart';
+import 'package:startech/HomeScreen/widgets/production_section.dart';
 import 'package:startech/utils/colors.dart';
 import 'package:startech/widgets/custom_button.dart';
 
@@ -41,77 +42,80 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            CarouselSlider.builder(
-              itemCount: sliders.length,
-              itemBuilder: (context, index, realIndex) {
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: AppColors.primaryColor,
-                    image: DecorationImage(
-                      image: AssetImage(sliders[index]),
-                      fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CarouselSlider.builder(
+                itemCount: sliders.length,
+                itemBuilder: (context, index, realIndex) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.primaryColor,
+                      image: DecorationImage(
+                        image: AssetImage(sliders[index]),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                );
-              },
-              options: CarouselOptions(
-                height: 200,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                reverse: false,
-                autoPlay: true,
-                autoPlayInterval: const Duration(seconds: 3),
-                autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                enlargeFactor: 0.3,
-                scrollDirection: Axis.horizontal,
+                  );
+                },
+                options: CarouselOptions(
+                  height: 200,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  scrollDirection: Axis.horizontal,
+                ),
               ),
-            ),
-            SizedBox(height: 22),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: AppColors.secondColor,
+              SizedBox(height: 22),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AppColors.secondColor,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Compare Products",
+                      style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                    ),
+                    SizedBox(height: 15),
+                    CustomField(
+                      hintText: 'Search and select product',
+                      icon: Icons.search,
+                    ),
+                    SizedBox(height: 15),
+                    CustomField(
+                      hintText: 'Search and select product',
+                      icon: Icons.search,
+                    ),
+                    SizedBox(height: 15),
+                    CustomButton(title: 'View Comparison',
+                    isBorderType: true,
+          
+                    ),
+          
+          
+                  ],
+                ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Compare Products",
-                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
-                  ),
-                  SizedBox(height: 15),
-                  CustomField(
-                    hintText: 'Search and select product',
-                    icon: Icons.search,
-                  ),
-                  SizedBox(height: 15),
-                  CustomField(
-                    hintText: 'Search and select product',
-                    icon: Icons.search,
-                  ),
-                  SizedBox(height: 15),
-                  CustomButton(title: 'View Comparison',
-                  isBorderType: true,
-
-                  ),
-
-
-                ],
-              ),
-            ),
-            // Compare product section
-            SizedBox(height: 22),
-
-          ],
+              // Compare product section
+              SizedBox(height: 22),
+              ProductionSection()
+          
+            ],
+          ),
         ),
       ),
     );
